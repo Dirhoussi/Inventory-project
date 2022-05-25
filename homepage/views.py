02ef60f4a4b3feb75f8267060abaff475e2325bf
Dirhoussi <mohamed.d@bodyo.com>
2022-05-25 22:57:54 +0200

    fiest commit

---
+++ homepage/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from inventory.models import Stock
+from transactions.models import SaleBill, PurchaseBill
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+
+
+
+class HomeView(View):
+    template_name = "home.html"
+    def get(self, request):        
+        labels = []
+        data = []        
+        stockqueryset = Stock.objects.filter(is_deleted=False).order_by('-quantity')
+        for item in stockqueryset:
+            labels.append(item.name)
+            data.append(item.quantity)
+        sales = SaleBill.objects.order_by('-time')[:3]
+        purchases = PurchaseBill.objects.order_by('-time')[:3]
+        context = {
+            'labels'    : labels,
+            'data'      : data,
+            'sales'     : sales,
+            'purchases' : purchases
+        }
+        return render(request, self.template_name, context)
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
